98a669eb36fecc15e790b9e8be634f50540fd03a
Souriat <mat.barnes@gmail.com>
2021-10-17 20:24:31 +0100

    added game function with while loop to run a 5 round game

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,4 +1,4 @@
-// Create an array with "rock" "paper" "scissors"
+// Create an array with "rock" "paper" "scissors" as options 
 
 const possibleOutcomes = [
     "Rock",
@@ -10,6 +10,11 @@ const possibleOutcomes = [
 //create variables for results
 let userResult
 let computerResult
+let resultMessage
+let rounds = 0
+let computerScore = 0
+let playerScore = 0
+let scoreMessage
 
 // Create a function called computerPlay
 function computerPlay() {
@@ -25,25 +30,99 @@ userResult = prompt("Rock, Paper or Scissors?");
 userResult = userResult.substring(0, 1).toUpperCase() + userResult.substring(1).toLowerCase();
 };
 
+
 //create a fuction that runs userPlay and computerPlay functions
-function playRound(playerSelection, computerSelection) {
-    playerSelection = userPlay();
+function playRound() {
+    userPlay();
     console.log(userResult);
-    computerSelection = computerPlay();
-    console.log(computerResult)
-    // compare results and return relevant win/loss text for each possible outcome
+    computerPlay();
+    console.log(computerResult);
+    printResults();
+    console.log(resultMessage);
+    scoreMessage = ("Your score is " + playerScore + " the computers score is " + computerScore);
+    console.log(scoreMessage);
+}
+
+// assign statements to win/loss outcomes
+
+function printResults() {
     if (userResult === "Paper" && computerResult === "Scissors") {
-            return ("You Lose, Computer picked " + computerResult + " and you picked " + userResult + ", " + computerResult + " beats " + userResult); 
+            rounds++
+            computerScore++
+            resultMessage = ("You Lose, Computer picked " + computerResult + " and you picked " + userResult + ", " + computerResult + " beats " + userResult); 
         }
 
     else if (userResult === "Paper" && computerResult === "Rock") {
-        return ("You Win!, Computer picked " + computerResult + " and you picked " + userResult + ", " + userResult + " beats " + computerResult);
+        rounds++
+        playerScore++
+        resultMessage = ("You Win!, Computer picked " + computerResult + " and you picked " + userResult + ", " + userResult + " beats " + computerResult);
      }
 
     else if (userResult === "Paper" && computerResult === "Paper") {
-        return ("Its a tie, Computer picked " + computerResult + " and you picked " + userResult + ", " + "try again!");
+        rounds++
+        resultMessage = ("Its a tie, Computer picked " + computerResult + " and you picked " + userResult + ", " + "try again!");
      }
 
-    };
+     else if (userResult === "Rock" && computerResult === "Paper") {
+        rounds++
+        computerScore++
+        resultMessage = ("You Lose, Computer picked " + computerResult + " and you picked " + userResult + ", " + computerResult + " beats " + userResult); 
+    }
+
+    else if (userResult === "Rock" && computerResult === "Scissors") {
+        rounds++
+        playerScore++
+        resultMessage = ("You Win!, Computer picked " + computerResult + " and you picked " + userResult + ", " + userResult + " beats " + computerResult);
+ }
+
+    else if (userResult === "Rock" && computerResult === "Rock") {
+        rounds++
+        resultMessage = ("Its a tie, Computer picked " + computerResult + " and you picked " + userResult + ", " + "try again!");
+ }
+
+      else if (userResult === "Scissors" && computerResult === "Rock") {
+        rounds++
+        computerScore++
+        resultMessage = ("You Lose, Computer picked " + computerResult + " and you picked " + userResult + ", " + computerResult + " beats " + userResult); 
+    }
+
+    else if (userResult === "Scissors" && computerResult === "Paper") {
+        rounds++
+        playerScore++
+        resultMessage = ("You Win!, Computer picked " + computerResult + " and you picked " + userResult + ", " + userResult + " beats " + computerResult);
+ }
+
+    else if (userResult === "Scissors" && computerResult === "Scissors") {
+        rounds++
+        resultMessage = ("Its a tie, Computer picked " + computerResult + " and you picked " + userResult + ", " + "try again!");
+    }
+};
 
 
+// create a function called game
+function game() {
+
+    // loop game 5 times
+
+        while (rounds <= 4) {
+        playRound()
+        }
+
+    // if 5 rounds completed return game over message
+
+        if (rounds >= 5) {
+            return ("Game Over type reset() to start again")
+        }
+    
+}
+
+function reset () {
+userResult = ""
+computerResult = ""
+resultMessage = ""
+rounds = 0
+computerScore = 0
+playerScore = 0
+scoreMessage = ""
+
+}
